b2d0716e901620984984edaa0456dfdf252e07aa
Bright <bright.andohh@gmail.com>
2024-05-02 21:12:25 +0000

    second commit

--- Blogs/posts/urls.py
+++ Blogs/posts/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("register.html", views.register, name="register"),
+    path("", views.register, name="register"),
     path("login.html", views.login, name="login"),
     path("blogspost.html", views.blogspost, name="blogsposts"),
     path("posts/<str:pk>", views.posts, name="posts")
